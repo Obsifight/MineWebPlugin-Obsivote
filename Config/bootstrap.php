9998ae03bd190b13d99736b2f1669e4ce08f1c17
Valentin <contact@eywek.fr>
2017-01-02 18:25:53 +0100

    edit vote rewards kits list

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,19 +3,19 @@
 //Liste des kits à donner à chaque reset & leur description
 
 Configure::write('Obsivote.kits', array(
-  'Voter1' => 'Stuff P4 Obsidian, Épée Sharpness 5 Fire 2 Unbreaking 3 en Titanium, Arc en Obsidian Power 5 Punch 1 Flame 1 Unbreaking 3 Infinity 1, 64 Flèches, 10 pommes d\'or, 20 pièces d\'or, 48 TNT, 10 XénoTNT, 4 Orbe de réparation, 16 enderpearls, 10 Stacks de bouteille d\'enchantement',
-  'Voter2' => 'Stuff P3 Obsidian, Épée Sharpness 4 Fire 2 en Titanium, Arc en Titanium Power 5 Flame 1 Unbreaking 1, 64 Flèches, 8 pommes d\'or, 20 pièces d\'or, 32 TNT, 8 XénoTNT, 3 Orbe de réparation, 16 enderpearls, 8 Stacks de bouteille d\'enchantement',
-  'Voter3' => 'Stuff P2 Obsidian, Épée Sharpness 3 Fire 2 en Titanium, Arc en Titanium Power 3 Flame 1, 64 Flèches, 6 pommes d\'or, 15 pièces d\'or, 24 TNT, 6 XénoTNT, 2 Orbe de réparation, 16 enderpearls, 6 Stacks de bouteille d\'enchantement',
-  'Voter4' => 'Stuff P1 Obsidian, Épée Sharpness 2 Fire 1 en Titanium, Arc en Titanium Power 1, 64 Flèches, 4 pommes d\'or, 10 pièces d\'or, 16 TNT, 4 XénoTNT, 1 Orbe de réparation, 16 enderpearls, 4 Stacks de bouteille d\'enchantement',
-  'Voter5' => 'Stuff Obsidian, Épée Sharpness 1 en Titanium, Arc en Titanium, 64 Flèches, 2 pommes d\'or, 5 pièces d\'or, 12 TNT, 2 XénoTNT, 16 enderpearls, 2 Stacks de bouteille d\'enchantement',
-  'Voter6' => 'Stuff P4 Titanium, Épée Sharpness 5 Fire 2 en Obsidian, Arc en Améthyste Power 5 Flame 1 Infinity 1, 64 Flèches, 1 pommes d\'or, 4 pièces d\'or, 8 TNT, 1 XénoTNT, 16 enderpearls, 1 Stacks de bouteille d\'enchantement',
-  'Voter7' => 'Stuff P3 Titanium, Épée Sharpness 5 Fire 2 en Obsidian, Arc en Améthyste Power 5 Flame 1 Infinity 1, 64 Flèches, 1 pommes d\'or, 4 pièces d\'or, 8 TNT, 1 XénoTNT, 16 enderpearls, 1 Stacks de bouteille d\'enchantement',
-  'Voter8' => '',
-  'Voter9' => '',
-  'Voter10' => '',
-  'Voter11' => '',
-  'Voter12' => '',
-  'Voter13' => '',
-  'Voter14' => '',
-  'Voter15' => ''
+  'Voter1' => 'Stuff P4 Titanium, Épée Sharpness 5 Fire 2 Unbreaking 3 en Titanium, Arc en Titanium Power 5 Punch 1 Flame 1 Unbreaking 3 Infinity 1, 1 Flèche, 48 TNT, 1 XénoTNT, 3 Orbe de réparation, 32 enderpearls, 10 Stacks de bouteille d\'enchantement, 12 cores neutres, 4 lingots de xénotium, 3 cookies d\'obsidian, 2 cookies de titanium, 1 cookie d\'améthyste, 1 cookie de garnet',
+  'Voter2' => 'Stuff P4 Titanium, Épée Sharpness 4 Fire 2 en Titanium, Arc en Titanium Power 4 Flame 1 Unbreaking 3, 64 Flèches, 32 TNT, 2 Orbe de réparation, 24 enderpearls, 8 Stacks de bouteille d\'enchantement, 11 cores neutres, 3 lingots de xénotium, 2 cookies d\'obsidian, 2 cookies de titanium, 1 cookie d\'améthyste, 1 cookie de garnet',
+  'Voter3' => 'Stuff P3 Titanium, Épée Sharpness 3 Fire 1 en Titanium, Arc en Titanium Power 3 Unbreaking 3, 64 Flèches, 32 TNT, 1 Orbe de réparation, 16 enderpearls, 6 Stacks de bouteille d\'enchantement, 10 cores neutres, 2 lingots de xénotium, 1 cookie d\'obsidian, 2 cookies de titanium, 2 cookies d\'améthyste, 1 cookie de garnet',
+  'Voter4' => 'Stuff P4 Améthyste, Épée Sharpness 5 Fire 2 en Améthyste, Arc en Améthyste Power 5 Flame 1 Unbreaking 3 Infinity 1, 1 Flèche, 16 TNT, 1 Orbe de réparation, 12 enderpearls, 4 Stacks de bouteille d\'enchantement, 1 lingot de xénotium, 9 cores neutres, 1 cookie d\'obsidian, 1 cookie de titanium, 2 cookies d\'améthyste, 2 cookies de garnet',
+  'Voter5' => 'Stuff P4 Garnet, Épée Sharpness 5 Fire 2 en Garnet, Arc en Garnet Power 5 Unbreaking 3 Infinity 1, 1 Flèche, 12 TNT, 8 enderpearls, 2 Stacks de bouteille d\'enchantement, 8 cores neutres, 10 lingots d\'obsidian, 1 cookie de titanium, 2 cookies d\'améthyste, 2 cookies de garnet',
+  'Voter6' => 'Stuff P4 Manganese, Épée Sharpness 5 Fire 2 en Manganese, Arc en Manganese Power 5 Flame 1 Unbreaking 3, 64 Flèches, 8 TNT, 6 enderpearls, 1 Stacks de bouteille d\'enchantement, 8 cores neutres, 5 lingots d\'obsidian, 2 cookies d\'améthyste, 3 cookies de garnet',
+  'Voter7' => '20 lingots d\'obsidian, 7 cores neutres',
+  'Voter8' => '16 lingots d\'obsidian, 6 cores neutres',
+  'Voter9' => '12 lingots d\'obsidian, 5 cores neutres',
+  'Voter10' => '8 lingots d\'obsidian, 4 cores neutres',
+  'Voter11' => '20 lingots de titanium, 3 cores neutres',
+  'Voter12' => '16 lingots de titanium, 2 cores neutres',
+  'Voter13' => '12 lingots de titanium, 2 cores neutres',
+  'Voter14' => '32 lingots d\'améthyste, 2 cores neutres',
+  'Voter15' => '32 lingots de garnet, 2 cores neutres'
 ));
